70e60b90e07cfc10e57bcd0ad98e7f0b9761ce1b
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-28 20:38:03 +0300

    solve(6kyu):Find The Parity Outlier

--- src/6kyu/Find The Parity Outlier.js
+++ src/6kyu/Find The Parity Outlier.js
@@ -3,8 +3,10 @@ function findOutlier(integers) {
     const evenArr = integers.filter((num) => num % 2 === 0);
     return evenArr.length < oddArr.length ? evenArr[0] : oddArr[0];
 }
+
 let res = findOutlier([1, 1, 0, 1, 1]);
 console.log('🚀 ~ res ➔', res);
+
 // Test.assertEquals(findOutlier([0, 1, 2]), 1);
 // Test.assertEquals(findOutlier([1, 2, 3]), 2);
 // Test.assertEquals(findOutlier([2, 6, 8, 10, 3]), 3);
