d2fe3de61891cdcb5488073278fa5cb398be90f2
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-09-06 21:03:03 +0300

    feat: update prototype

---
+++ src/JS/prototype.js
@@ -0,0 +1,28 @@
+function Vehicle(type) {
+    this.type = type;
+}
+
+Vehicle.prototype.move = function () {
+    console.log(`${this.type} is moving`);
+};
+
+function Car(type) {
+    Vehicle.call(this, type)
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car; // Восстанавливаем конструктор
+
+Car.prototype.drive = function () {
+    console.log(`This ${this.type} is driving`);
+};
+
+const car1 = new Car('Sedan');
+const car2 = new Car('SUV');
+
+car1.move();  // "This car is moving"
+car1.drive(); // "This car is driving"
+
+car2.move();  // "This car is moving"
+car2.drive(); // "This car is driving"
+
